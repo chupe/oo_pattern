a15dc813488465dd077d155d587b46524ac96a5d
chupe <velahavle@gmail.com>
2018-02-09 11:47:25 +0100

    First implementation

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -18,7 +18,7 @@ impl Post {
     }
 
     pub fn content(&self) -> &str {
-        ""
+        self.state.as_ref().unwrap().content(&self)
     }
 
     pub fn request_review(&mut self) {
@@ -26,10 +26,20 @@ impl Post {
             self.state = Some(s.request_review())
         }
     }
+
+    pub fn publish(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
 }
 
 trait State {
     fn request_review(self: Box<Self>) -> Box<State>;
+    fn approve(self: Box<Self>) -> Box<State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
 }
 
 struct Draft {}
@@ -38,6 +48,9 @@ impl State for Draft {
     fn request_review(self: Box<Self>) -> Box<State> {
         Box::new(PendingReview {})
     }
+    fn approve(self: Box<Self>) -> Box<State> {
+        self
+    }
 }
 
 struct PendingReview {}
@@ -45,6 +58,23 @@ struct PendingReview {}
 impl State for PendingReview {
     fn request_review(self: Box<Self>) -> Box<State> {
         self
+    }
+    fn approve(self: Box<Self>) -> Box<State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<State> {
+        self
+    }
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
     }
 }
 
